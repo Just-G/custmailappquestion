a9fc0ec0e4d335da74037112379b1ff2dd9d1f2a
Just-G <53385979+Just-G@users.noreply.github.com>
2021-08-22 19:44:17 +0700

    Complete MountainCustomer.java

--- src/customermailapplication/MountainCustomer.java
+++ src/customermailapplication/MountainCustomer.java
@@ -11,8 +11,13 @@ package customermailapplication;
  * @author sarun
  */
 //make this class a subclass of customer
-public class MountainCustomer  {
+public class MountainCustomer extends Customer  {
 
+    @Override
+    public String createMail() {
+        return "MountainCustomer";
+    }
+    
     //Complete this class
     
 }
